b522db7d369df9a6e76ffe085a125520edad471e
WashinChuen <73007799+WashinChuen@users.noreply.github.com>
2021-06-12 11:52:08 +0900

    Created Board and Tiles
    To do: Find the correct positions

--- Assets/BoardLayoutScript.cs
+++ Assets/BoardLayoutScript.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class BoardLayoutScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //int[] intx;
+    //int[] inty;
+    Vector2[,] boardPositions;
+    [SerializeField] GameObject whiteTile = null;
+    [SerializeField] GameObject blackTile = null;
+    Vector2 boardOffset = new Vector2(800, 800);
+    GameObject tempTile = null;
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        boardPositions = new Vector2[8, 8];
+        for (int x = 0; x < 8; x++)
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                //boardPositions[x, y] = new Vector2((x * 175) + 90, (y * 175) + 90);
+                boardPositions[x, y] = new Vector2((x * 100) + 50, (y * 100) + 50);
+                tempTile = Instantiate(blackTile, boardPositions[x, y] - boardOffset, Quaternion.identity);
+                //tempTile.transform.position = boardPositions[x, y] - boardOffset;
+                tempTile.transform.SetParent(this.gameObject.transform);
+                //tempTile.transform.localScale = Vector3.one;
+            }
+        }
     }
 }
